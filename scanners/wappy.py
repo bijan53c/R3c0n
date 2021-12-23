374ea84be35f1f731883273988706b0fbdbff074
Bijan Agharafiei <63957104+bijan53c@users.noreply.github.com>
2021-12-23 05:42:09 -0500

    fix update
    wappy will add https tp the target domain so it can use it as a URL

--- scanners/wappy.py
+++ scanners/wappy.py
@@ -3,14 +3,17 @@ import json
 file = open('targeturl.txt','r')
 readfile = file.read()
 TargetURL = readfile.strip()
-print ("target : ",TargetURL)
-url = 'https://api.wappalyzer.com/v2/lookup/?urls=' + TargetURL 
-headers = {'x-api-key' : '<wapplyzer API key>'}
+url = "https://"+TargetURL
+#targeturlHTTPS = "https://" + TargetURL
+#targeturlHTTP = "http://" + TargetURL
+print ("target : ",url)
+url = 'https://api.wappalyzer.com/v2/lookup/?urls=' + url 
+headers = {'x-api-key' : '<API key>'}
 r = requests.get(url, headers=headers)
 response_file = r.json()
 clean_response = json.dumps(response_file,sort_keys=True, indent=4)
 print(clean_response)
 resultfile = open('wappyresult.txt','w')
-resultfile.write(clean_response)
+resultfile.writelines(clean_response)
 file.close()
 resultfile.close()
